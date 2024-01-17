776ae3ef65841bbb7b3937d955c8ec9a5326f5ae
Steve Gattuso <steve@stevegattuso.me>
2024-01-17 22:56:48 +0100

    try a simpler planet.yaml format

--- scripts/fetch_blogs.rb
+++ scripts/fetch_blogs.rb
@@ -26,7 +26,7 @@ def fetch_posts(fellow)
        "title" => title,
        "url" => link,
        "fellow" => fellow,
-       "description" => content,
+       "description" => "Read the full post <a href=\"${content}\">here</a>.",
        "published_at" => published_at}
     end
   end
