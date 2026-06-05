c522ed71446c28ce36d99649d3055687e5a7573c
Steve Gattuso <steve@stevegattuso.me>
2026-06-05 15:49:03 -0400

    Automated deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ git pull
 # I know this is jank to hard code but it's only running on my server for now
 # and I don't have time to do this the right way right this second. pls forgive
 # me, for I have sinned.
-rv run ./scripts/fetch_blogs.rb
+/home/steve/.local/bin/rv run ./scripts/fetch_blogs.rb
 
 git commit -am "Automated deploy"
 git push
